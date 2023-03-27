4838effa45f62be53a6cfdc9c4e16f5f66cb1dec
Estrella Mooney <estrellamooney96@gmail.com>
2023-03-27 20:31:10 +1300

    Working on route

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,27 +1,14 @@
 import { useState, useEffect } from 'react'
 import { getAllImages, getGreeting } from '../api/apiClient'
-// import { getImages } from '../api/apiImages'
+// import { Routes, Route } from 'react-router-dom'
+import { ImageDetails } from '../../common/Images'
 import Images from './Images'
 
 function App() {
-  const [images, setWidgets] = useState([] as Types.Images[]) // we hold our data in a state, and use setWidgets to change this state
-  useEffect(() => {
-    getAllImages()
-      .then((wodget) => {
-        setWidgets(wodget) // once we have our data, we can set our state to hold it
-      })
-      .catch((err) => alert(err.message))
-  }, []) // Using [] means we only useEffect when the page is first loaded --> [ fetchWidgets ] //focus on particular element
-
-
   return (
-    <>
-      <p>Hi</p>
+    <div className="App">
       <Images />
-      {/* <Routes>
-        <Route path="/" element={<getImages />} />
-      </Routes> */}
-    </>
+    </div>
   )
 }
 
--- client/components/Images.tsx
+++ client/components/Images.tsx
@@ -1,18 +1,59 @@
 import { useState, useEffect } from 'react'
 import { ImageDetails } from '../../common/Images'
-// import { getImages } from '../api/apiImages'
+import { getAllImages } from '../api/apiClient'
 
-interface Props {
-  imageProp: ImageDetails
-}
+function Images() {
+  const [images, setImages] = useState<ImageDetails[]>([])
 
-function Images(props: Props) {
-  const { description, country_name } = props.imageProp
+  useEffect(() => {
+    getAllImages()
+      .then((imageArr) => {
+        setImages(imageArr)
+      })
+      .catch((err) => alert(err.message))
+  }, [])
+
   return (
-    <div className="widget-container">
-      <h1>{country_name}</h1>
-      <p>Price: {description}</p>
+    <div>
+      {images.map((image) => (
+        <div key={image.id} className="widget-container">
+          <h1>{image.country_name}</h1>
+          <p>Description: {image.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
+
 export default Images
+
+// import { useState, useEffect } from 'react'
+// import { ImageDetails } from '../../common/Images'
+// import { getAllImages } from '../api/apiClient'
+// import * as Models from '../../common/Images'
+
+// export function Images(image: ImageDetails) {
+//   // const { description, country_name } = props.imageProp
+
+//   const [images, setImages] = useState<ImageDetails[]>([])
+
+//   useEffect(() => {
+//     getAllImages()
+//       .then((imageArr) => {
+//         setImages(imageArr)
+//       })
+//       .catch((err) => alert(err.message))
+//   }, [])
+
+//   return (
+//     <div>
+//       {images.map((image) => (
+//         <div key={image.id} className="widget-container">
+//           <h1>{image.country_name}</h1>
+//           <p>Description: {image.description}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+// export default Images
